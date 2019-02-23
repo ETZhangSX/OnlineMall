1043d5182ea0cacd14c633251a6e4be0a2da0db2
ETZhang <541891089@qq.com>
2019-02-23 11:11:26 +0800

    添加了搜索功能

--- miniprogram/pages/searchResult/searchResult.js
+++ miniprogram/pages/searchResult/searchResult.js
@@ -6,7 +6,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    result: []
+    result: [],
+    search_Key: null
   },
 
   /**
@@ -16,8 +17,42 @@ Page({
     var that = this;
     searchKey = options.searchKey;
     console.log(searchKey);
+    that.setData ({
+      search_Key: searchKey
+    })
+    that.resultShow();
   },
 
+  resultShow: function(e) {
+    var that = this;
+    const db = wx.cloud.database()
+    // const _ = db.command
+    db.collection('goods').where({
+      title: db.RegExp({
+        regexp: searchKey,
+        options: 'i',
+      })
+    })
+      .get({
+        success(res) {
+          console.log(res.data)
+          that.setData ({
+            result: res.data
+          })
+
+        }
+      })
+  },
+
+  onSearch: function(e) {
+    var that = this;
+    searchKey = e.detail;
+    console.log(searchKey);
+    that.setData ({
+      search_Key: searchKey
+    })
+    that.resultShow();
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -65,5 +100,18 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  //跳转到详情页
+  catchTapCategory: function (e) {
+    var that = this;
+    var goodsId = e.currentTarget.dataset.goodsid;
+    var dbName = e.currentTarget.dataset.dbname;
+    console.log('goodsId:' + goodsId);
+    //新增商品用户点击数量
+    // that.goodsClickShow(goodsId);
+    //跳转商品详情
+    wx.navigateTo({
+      url: '../detail/detail?goodsId=' + goodsId + '&dbName=' + dbName
+    })
+  },
 })
